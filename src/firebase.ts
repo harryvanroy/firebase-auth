200600a51545b5c16eab1a8e4fd0223e7c00d45c
harryvanroy <harrison.vanroy007@gmail.com>
2022-01-08 15:06:39 +1000

    f

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,14 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCAqa3khm5CIZHVZOmCTRFTol1HD2N265I",
-  authDomain: "music-app-43af4.firebaseapp.com",
-  projectId: "music-app-43af4",
-  storageBucket: "music-app-43af4.appspot.com",
-  messagingSenderId: "305322615412",
-  appId: "1:305322615412:web:99abb56a6c0c1b427e6721",
-};
+const firebaseConfig = {};
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
